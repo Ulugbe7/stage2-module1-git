50ba62f0d083fc4e1f2ef7a1fee9b5d0de951ce9
ulugbek <ulugbekyusupov272@gmail.com>
2024-02-11 23:51:09 +0500

    finish2

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/Ulugbe7/mjcGitTask";
+    public static String REPO_LINK = "https://github.com/mmustafoyev/repository";
 }
